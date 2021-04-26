b1aa58e3f54f8879d0504c5466d5b4673b227ac7
antdbs <antoinedubois14@gmail.com>
2021-04-26 10:06:11 +0200

    update app templating

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from elasticsearch import Elasticsearch
-"""from bot import *
-import schedule
-import time
 
+
+app = Flask(__name__)
+
+"""
 LOCAL = False
 
 es_client = Elasticsearch(hosts=["localhost" if LOCAL else "elasticsearch"])
@@ -13,26 +14,34 @@ document = {
     "description":"Contains all of the porfil of the application, the objectif is to provide the best profile for our user.",
     "algo_type":"Web application"
 }
-
-
-
 cpt = 1
  """
 
-app = Flask(__name__)
+data = {
+    "name" : "Laury",
+    "age" : "19",
+    "picture" : "https://scontent-cdt1-1.cdninstagram.com/v/t51.2885-19/s150x150/70397507_2154880494806199_5101106879065489408_n.jpg?tp=1&_nc_ht=scontent-cdt1-1.cdninstagram.com&_nc_ohc=gkUkJ3UgE78AX9HC2GJ&edm=ABfd0MgAAAAA&ccb=7-4&oh=ca21a6541933b37981be5747c02538c4&oe=60AB2738&_nc_sid=7bff83",
+    "instagram" : "laury_rdg",
+    "tags": ['Musée', 'Sports', 'Voyage', 'Art', 'Astrologie'] 
+}
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    name = data['name']
+    age = data['age']
+    photo = data['picture']
+    instagram = data['instagram']
+    tags = data['tags']
+    return render_template('index.html', name=name, age=age, photo=photo, instagram=instagram, tags = tags)
 
-@app.route('/marketplace')
+@app.route('/marcketplace')
 def marketplace():
-    return render_template('marketplace.html')
-
-@app.route('/scrapyBotLaunch')
-def scrapyBotLaunch():
-    
-    return "Scrapping done"
+    name = data['name']
+    age = data['age']
+    photo = data['picture']
+    instagram = data['instagram']
+    tags = data['tags']
+    return render_template('marcketplace.html', name=name, age=age, photo=photo, instagram=instagram, tags = tags)
 
 if __name__ == '__main__':
     app.run(debug=True)
